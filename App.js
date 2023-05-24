ce9e6df833c05a45fbdefd56dbe5f04434502135
Deepkamal yadav <dkits1995@gmail.com>
2023-05-24 15:46:00 +0530

    form homepage

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigator from "./Components/StackNavigator";
+import { ModalPortal } from "react-native-modals";
 
 export default function App() {
   return (
     <>
       <StackNavigator />
+      <ModalPortal />
     </>
   );
 }
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -14,10 +14,23 @@ import Header from "../Components/Header";
 import { EvilIcons } from "@expo/vector-icons";
 import { Fontisto } from "@expo/vector-icons";
 import DatePicker from "react-native-date-ranges";
+import {
+  BottomModal,
+  ModalButton,
+  ModalContent,
+  ModalFooter,
+  ModalTitle,
+  SlideAnimation,
+} from "react-native-modals";
+
 // home layout
 const HomeScreen = () => {
   const navigation = useNavigation();
   const [selectedDates, setSelectedDates] = useState();
+  const [selectedrooms, setSelectedrooms] = useState(1);
+  const [selectedadults, setSelectedadults] = useState(2);
+  const [selectedchilderen, setSelectedchildren] = useState(0);
+  const [modalVisibale, setModalVisibale] = useState(false);
   console.log(selectedDates);
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -59,85 +72,371 @@ const HomeScreen = () => {
     );
   };
   return (
-    <View>
-      <Header />
-      <ScrollView>
-        <View
-          style={{
-            margin: 20,
-            borderColor: "#FFC72C",
-            borderWidth: 3,
-            borderRadius: 6,
-          }}
-        >
-          {/*  destination */}
-          <Pressable
+    <>
+      <View>
+        <Header />
+        <ScrollView>
+          <View
             style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-              paddingHorizontal: 10,
+              margin: 20,
               borderColor: "#FFC72C",
-              borderWidth: 2,
-              paddingVertical: 15,
+              borderWidth: 3,
+              borderRadius: 6,
             }}
           >
-            <EvilIcons name="search" size={24} color="black" />
-            <TextInput placeholder="Enter Your Destination"></TextInput>
-          </Pressable>
-          {/* Selected Dates */}
-          <Pressable
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-              paddingHorizontal: 10,
-              borderColor: "#FFC72C",
-              borderWidth: 2,
-              paddingVertical: 15,
-            }}
-          >
-            <Fontisto name="date" size={24} color="black" />
-            <DatePicker
+            {/*  destination */}
+            <Pressable
               style={{
-                width: 350,
-                height: 30,
-                borderRadius: 0,
-                borderWidth: 0,
-                borderColor: "tr",
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
               }}
-              customStyles={{
-                placeholderText: {
-                  fontSize: 15,
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginRight: "auto",
-                },
-                headerStyle: {
-                  backgroundColor: "#003580",
-                },
-                contenttext: {
-                  fontSize: 15,
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginRight: "auto",
-                },
+            >
+              <EvilIcons name="search" size={24} color="black" />
+              <TextInput
+                placeholderTextColor="black"
+                placeholder="Enter Your Destination"
+              ></TextInput>
+            </Pressable>
+            {/* Selected Dates */}
+            <Pressable
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
               }}
-              selectedBgColor="#0047AB"
-              customButton={(onConfirm)=>customButton(onConfirm)}
-              onConfirm={(startDate,endDate)=>setSelectedDates(startDate,endDate)}
-              allowFontScaling={false}
-              placeholder={"Apr 27, 2018 → Jul 10, 2018"}
-              mode={"range"}
+            >
+              <Fontisto name="date" size={24} color="black" />
+              <DatePicker
+                style={{
+                  width: 350,
+                  height: 30,
+                  borderRadius: 0,
+                  borderWidth: 0,
+                  borderColor: "tr",
+                }}
+                customStyles={{
+                  placeholderText: {
+                    fontSize: 15,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: "auto",
+                  },
+                  headerStyle: {
+                    backgroundColor: "#003580",
+                  },
+                  contenttext: {
+                    fontSize: 15,
+                    flexDirection: "row",
+                    alignItems: "center",
+                    marginRight: "auto",
+                  },
+                }}
+                selectedBgColor="#0047AB"
+                customButton={(onConfirm) => customButton(onConfirm)}
+                onConfirm={(startDate, endDate) =>
+                  setSelectedDates(startDate, endDate)
+                }
+                allowFontScaling={false}
+                placeholder={"Apr 27, 2018 → Jul 10, 2018"}
+                mode={"range"}
+              />
+            </Pressable>
+            {/* Rooms and Guests */}
+            <Pressable
+              onPress={() => setModalVisibale(!modalVisibale)}
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
+              }}
+            >
+              <Ionicons name="person-add-outline" size={24} color="black" />
+              <TextInput
+                placeholderTextColor="red"
+                placeholder={`${selectedrooms} room + ${selectedadults} adults + ${selectedchilderen} children`}
+              ></TextInput>
+            </Pressable>
+            {/* Search Button */}
+            <Pressable
+              style={{
+                paddingHorizontal: 10,
+                borderColor: "#FFC72C",
+                borderWidth: 2,
+                paddingVertical: 15,
+                backgroundColor: "#2a52be",
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 15,
+                  fontWeight: "500",
+                  color: "white",
+                }}
+              >
+                Search
+              </Text>
+            </Pressable>
+         
+          </View>
+          <Text style={{marginHorizontal:20,fontSize:17,fontWeight:"500"}}>Travel More Spend Less</Text>
+        </ScrollView>
+      </View>
+      <BottomModal
+        swipeThreshold={200}
+        onBackdropPress={() => setModalVisibale(!modalVisibale)}
+        swipeDirection={["up", "down"]}
+        footer={
+          <ModalFooter>
+            <ModalButton
+              text="Apply"
+              style={{
+                marginBottom: 20,
+                color: "white",
+                backgroundColor: "#003580",
+              }}
+              onPress={() => setModalVisibale(!modalVisibale)}
             />
-          </Pressable>
-          {/* Rooms and Guests */}
-          <Pressable></Pressable>
-          {/* Search Button */}
-          <Pressable></Pressable>
-        </View>
-      </ScrollView>
-    </View>
+          </ModalFooter>
+        }
+        modalTitle={<ModalTitle title="Select rooms and guests" />}
+        modalAnimation={
+          new SlideAnimation({
+            slideFrom: "bottom",
+          })
+        }
+        onHardwareBackPress={() => setModalVisibale(!modalVisibale)}
+        visible={modalVisibale}
+        onTouchOutside={() => setModalVisibale(!modalVisibale)}
+      >
+        <ModalContent style={{ width: "100%", height: 310 }}>
+          {/* rooms */}
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginVertical: 15,
+            }}
+          >
+            <Text style={{ fontSize: 16, fontWeight: "500" }}>Rooms</Text>
+            <Pressable
+              style={{ flexDirection: "row", alignItems: "center", gap: 20 }}
+            >
+              <Pressable
+                onPress={() => setSelectedrooms(Math.max(1, selectedrooms - 1))}
+                style={{
+                  width: 26,
+                  height: 26,
+                  borderRadius: 13,
+                  borderColor: "#BEBEBE",
+                  backgroundColor: "#E0E0E0",
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: 20,
+                    fontWeight: "600",
+                    paddingHorizontal: 6,
+                  }}
+                >
+                  -
+                </Text>
+              </Pressable>
+              <Pressable>
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: 18,
+                    fontWeight: "500",
+                    paddingHorizontal: 6,
+                  }}
+                >
+                  {selectedrooms}
+                </Text>
+              </Pressable>
+              <Pressable
+                onPress={() => setSelectedrooms((c) => c + 1)}
+                style={{
+                  width: 26,
+                  height: 26,
+                  borderRadius: 13,
+                  borderColor: "#BEBEBE",
+                  backgroundColor: "#E0E0E0",
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: 20,
+                    fontWeight: "600",
+                    paddingHorizontal: 6,
+                  }}
+                >
+                  +
+                </Text>
+              </Pressable>
+            </Pressable>
+          </View>
+          {/* adults */}
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginVertical: 15,
+            }}
+          >
+            <Text style={{ fontSize: 16, fontWeight: "500" }}>Adults</Text>
+            <Pressable
+              style={{ flexDirection: "row", alignItems: "center", gap: 20 }}
+            >
+              <Pressable
+                onPress={() =>
+                  setSelectedadults(Math.max(1, selectedadults - 1))
+                }
+                style={{
+                  width: 26,
+                  height: 26,
+                  borderRadius: 13,
+                  borderColor: "#BEBEBE",
+                  backgroundColor: "#E0E0E0",
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: 20,
+                    fontWeight: "600",
+                    paddingHorizontal: 6,
+                  }}
+                >
+                  -
+                </Text>
+              </Pressable>
+              <Pressable>
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: 18,
+                    fontWeight: "500",
+                    paddingHorizontal: 6,
+                  }}
+                >
+                  {selectedadults}
+                </Text>
+              </Pressable>
+              <Pressable
+                onPress={() => setSelectedadults((c) => c + 1)}
+                style={{
+                  width: 26,
+                  height: 26,
+                  borderRadius: 13,
+                  borderColor: "#BEBEBE",
+                  backgroundColor: "#E0E0E0",
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: 20,
+                    fontWeight: "600",
+                    paddingHorizontal: 6,
+                  }}
+                >
+                  +
+                </Text>
+              </Pressable>
+            </Pressable>
+          </View>
+          {/* children */}
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginVertical: 15,
+            }}
+          >
+            <Text style={{ fontSize: 16, fontWeight: "500" }}>Childrens</Text>
+            <Pressable
+              style={{ flexDirection: "row", alignItems: "center", gap: 20 }}
+            >
+              <Pressable
+                onPress={() =>
+                  setSelectedchildren(Math.max(0, selectedchilderen - 1))
+                }
+                style={{
+                  width: 26,
+                  height: 26,
+                  borderRadius: 13,
+                  borderColor: "#BEBEBE",
+                  backgroundColor: "#E0E0E0",
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: 20,
+                    fontWeight: "600",
+                    paddingHorizontal: 6,
+                  }}
+                >
+                  -
+                </Text>
+              </Pressable>
+              <Pressable>
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: 18,
+                    fontWeight: "500",
+                    paddingHorizontal: 6,
+                  }}
+                >
+                  {selectedchilderen}
+                </Text>
+              </Pressable>
+              <Pressable
+                onPress={() => setSelectedchildren((c) => c + 1)}
+                style={{
+                  width: 26,
+                  height: 26,
+                  borderRadius: 13,
+                  borderColor: "#BEBEBE",
+                  backgroundColor: "#E0E0E0",
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: 20,
+                    fontWeight: "600",
+                    paddingHorizontal: 6,
+                  }}
+                >
+                  +
+                </Text>
+              </Pressable>
+            </Pressable>
+          </View>
+        </ModalContent>
+      </BottomModal>
+    </>
   );
 };
 
