9b1c40c6fe2121d1e37998d04f58f03e50433450
Deepkamal yadav <dkits1995@gmail.com>
2023-06-18 21:23:40 +0530

    add

--- Components/PropertyCard.js
+++ Components/PropertyCard.js
@@ -1,6 +1,6 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
-// impotent things important things time on  the timecd  into
+// impotent things important things time on  the timecd  into the 
 const PropertyCard = () => {
   return (
     <View>
